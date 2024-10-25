805c85e1c0c36e41e27ec7c0e0f5d0afd05ff33d
giuseppe <giuseppefernando09@gmail.com>
2024-10-24 22:46:02 -0300

    Tela de Pagina de Final feita

--- LandingPage/src/App.jsx
+++ LandingPage/src/App.jsx
@@ -13,11 +13,11 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 
+
 function App() {
   return (
     <>
       <Background />
-
       <Header />
       <TextTelaInicial />
       <Sobre />
